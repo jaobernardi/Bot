2aefc32bba99f45ee9614f06566621ca10bea7d6
Bernard <joao_Bernardi@outlook.com>
2018-11-29 18:22:38 -0200

    V.1

---
+++ BotSpy.py
@@ -0,0 +1,40 @@
+import discord
+import asyncio
+import sys
+import json
+import time
+import random
+import requests
+import dhooks
+from pathlib import Path
+client = discord.Client()
+hook = dhooks.Webhook('https://discordapp.com/api/webhooks/516092910081409058/dQ1YJOs3qwD57lM6CH9nhChJyhVO39Oc1YchGpUQE3f1qvpHIu8EXptI-_qlXUVxBjkG')
+msg = 'PANDA CORNUDA\nPANDA CORNUDA\nPANDA CORNUDA\nPANDA CORNUDA\nPANDA CORNUDA\nPANDA CORNUDA\nPANDA CORNUDA\nPANDA CORNUDA\nPANDA CORNUDA\nPANDA CORNUDA\nPANDA CORNUDA\nPANDA CORNUDA\nPANDA CORNUDA\nPANDA CORNUDA\nPANDA CORNUDA\nPANDA CORNUDA\nPANDA CORNUDA\nPANDA CORNUDA\n'
+def ServerInfo(id):
+	server_path = Path(f"Servers/{id}.json")
+	if server_path.exists():
+		info = json.load(open(f'Servers/{id}.json'))
+		return info
+	else:
+		with open(f'Servers/{id}.json', 'w') as f:
+			x = json.load(open(f'ServerBase.json'))
+			x['id'] = id
+			f.write(json.dumps(x))
+			return x
+@client.event
+async def on_message(message):
+	server_info = ServerInfo(f"{message.server.id}")
+	prefix = server_info['prefix']
+	command = message.content.lower().startswith
+	if command(prefix):
+		args = message.content.split(" ")
+		if command(prefix + "help"):
+			await client.send_message(message.channel, "Version: ALPHA 1 SNPSHT:BG01")
+		
+	
+@client.event
+async def on_ready():
+	await client.change_status(game=discord.Game(name='Jogos! ;)'))
+	print(f"[+] Logged as {client.user.name}")
+client.run(f"NTE2MTE2MTkzMDQ0NzI1Nzgw.Dtu-yg.8y-5tV-e4qqmE9Uhub5sWio4w7g")
+	
