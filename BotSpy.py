a79f47b2d77cd25bd0b53ed88394f9ab6113d0c4
Bernard <joao_Bernardi@outlook.com>
2018-11-29 21:11:29 -0200

    V.6

--- BotSpy.py
+++ BotSpy.py
@@ -8,22 +8,23 @@ import requests
 import dhooks
 from pathlib import Path
 client = discord.Client()
-
 hook = dhooks.Webhook('https://discordapp.com/api/webhooks/516092910081409058/dQ1YJOs3qwD57lM6CH9nhChJyhVO39Oc1YchGpUQE3f1qvpHIu8EXptI-_qlXUVxBjkG')
+help_embed=discord.Embed(title="Help me!", description="Aqui estão todos os comandos disponivéis!")
+help_embed.add_field(name="Misc", value=f"{prefix}say -» *Faça eu dizer algo!*\n{prefix}avatar -» *Veja seu avatar ou de outro usuario ;)*\n{prefix}perfil -» *mostra seu perfil ou o de outro usuario :)*\n{prefix}say -»", inline=False)
+help_embed.add_field(name="Administrativo", value="n", inline=False)
+help_embed.add_field(name="Musica", value="n", inline=False)
+help_embed.add_field(name="Comandos Personalizados", value="n", inline=False)
 def ServerInfo(id):
 	server_path = Path(f"Servers/{id}.json")
 	if server_path.exists():
 		info = json.load(open(f'Servers/{id}.json'))
-		return info
-		
+		return info		
 	else:
 		with open(f'Servers/{id}.json', 'w') as f:
 			x = json.load(open(f'ServerBase.json'))
 			x['id'] = id
 			f.write(json.dumps(x))
 			return x
-			
-			
 @client.event
 async def on_message(message):
 	server_info = ServerInfo(f"{message.server.id}")
