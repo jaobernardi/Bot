bc879575d70d776d359e716cac5e0e406f6fdcfd
Bernard <joao_Bernardi@outlook.com>
2018-11-30 20:36:35 -0200

    V.6

--- BotSpy.py
+++ BotSpy.py
@@ -44,7 +44,7 @@ async def on_message(message):
 			await client.send_message(message.channel, embed=help_embed)
 		if command(prefix + "wegotthem"):
 			print(args[1])
-			if args[1] == f'Key':
+			if args[1] == f'{Key}':
 				if message.server.id != '513142267654176784':
 					Key = KeyGen()
 					players = []
