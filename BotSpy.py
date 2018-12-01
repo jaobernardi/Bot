425da98d9d68870a8e20522f555d9c8f27562a3e
Bernard <joao_Bernardi@outlook.com>
2018-12-01 21:11:52 -0200

    V.6

--- BotSpy.py
+++ BotSpy.py
@@ -69,7 +69,7 @@ async def on_message(message):
 	server_info = server.getInfo(f"{message.server.id}")
 	prefix = server_info['prefix']
 #Admin Commands
-	await Commands.handler(message, client)
+	Commands.handler(message, client) 
 @client.event
 async def on_ready():
 	await client.change_status(game=discord.Game(name='Jogos! ;)'))
