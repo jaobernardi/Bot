94d925d4bdc844e7e8962d4e39aa97031bb8f29f
Bernard <joao_Bernardi@outlook.com>
2018-12-01 21:09:57 -0200

    V.6

--- Commands.py
+++ Commands.py
@@ -8,22 +8,6 @@ import requests
 import dhooks
 from pathlib import Path
 import string
-def ComoUsar(command):
-	if command in regular_commands:
-		args, exempleagrs = commands.info(command)
-	embed=discord.Embed(title=f"Como usar", description=f"Aqui está a sintaxe do comando **{command}**\n**Uso Correto**: `{command} {args}`\n**Exemplo de uso**: `{command} {exempleagrs}`")
-	return embed
 def handler(message, client):
 	if message.content == "oi":
 		return await client.send_message(message.channel, "Teste module top")
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
