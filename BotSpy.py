b44552ccf4736d0d48fd4f4b7b1b3777d60d6641
Bernard <joao_Bernardi@outlook.com>
2018-12-01 13:06:11 -0200

    V.6

--- BotSpy.py
+++ BotSpy.py
@@ -84,7 +84,7 @@ async def on_message(message):
 		if command(prefix + "say"):
 			try:
 				say = " ".join(args[1:])
-				if len(say) > 1:
+				if len(say) => 1:
 					say=discord.Embed(color=0x2C2F33, title=" ", description=f"{say}")
 					await client.send_message(message.channel, embed=say)
 				else:
@@ -114,7 +114,7 @@ async def on_message(message):
 						await client.send_message(message.channel, "```css\n#Error\n\n[ 0 ] This command is not allowed in this server.\n\nLenny #EC:1001\n```")
 			except IndexError:
 				if message.server.id == '513142267654176784':
-					await client.send_message(message.channel, embed=ComoUsar("wegotthem"))
+					await client.send_message(message.channel, embed=ComoUsar("wegotthem", "", ""))
 #Admin Commands
 	
 					
