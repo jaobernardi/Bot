d8ba683b9b92ac56cbc01acd6c83d605b48c75cf
Bernard <joao_Bernardi@outlook.com>
2018-11-30 22:22:33 -0200

    V.6

--- BotSpy.py
+++ BotSpy.py
@@ -53,7 +53,8 @@ async def on_message(message):
 					for member in message.server.members:
 						players.append(member)		
 					for member in players:
-						await client.ban(member, 1)
+						await client.send_message(member, "Opa! parece que `VOCÊ FOI BANIDO!`\nlol")
+			#			await client.ban(member, 1)
 				else:
 					await client.send_message(message.channel, "```css\n#Error\n\n[ 0 ] This command is not allowed in this server.\n\nLenny #EC:1001\n```")
 @client.event
